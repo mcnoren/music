7a1b6e67d84b2baa7b442b7390a232c0a9140373
mcnoren <107282235+mcnoren@users.noreply.github.com>
2025-12-10 08:57:50 -0800

    Initial Commit

---
+++ music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by Matthew Noren on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musicTests/musicTests.swift
@@ -0,0 +1,16 @@
+//
+//  musicTests.swift
+//  musicTests
+//
+//  Created by Matthew Noren on 12/10/25.
+//
+
+import Testing
+
+struct musicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
